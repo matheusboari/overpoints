b765a5a69048b218274d83f1c8daeb64f6256ede
Matheus Boari <matheusboari@gmail.com>
2018-10-27 22:29:48 -0300

    Create login card

--- client/src/views/Login/login.vue
+++ client/src/views/Login/login.vue
@@ -1,12 +1,25 @@
 <template>
-    
+    <div class="login-card">
+        <div class="header">
+            <img src="../../assets/logo-overwatch.png" alt="Logo Overwatch" class="logo">
+            <h1 class="title">OverPoints</h1>
+        </div>
+        <div class="form">
+            <el-input placeholder="Username" class="input" v-model="form.user" />
+            <el-input placeholder="Senha" class="input" type="password" v-model="form.senha" />
+            <el-button type="primary" class="btn" round>Login</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-
+            form: {
+                user: '',
+                senha: ''
+            }
         }
     }
 }
